ff1acca8b98dc4aa71c8ad20bb67afc539ed0e3c
Charles Bryant <cwbryant25@yahoo.com>
2024-02-27 12:49:40 -0800

    App,jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
